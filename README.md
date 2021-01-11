387e9f64e6d96508b397408de33f2549a97b6752
nandyme <fernandomellone2@gmail.com>
2021-01-11 01:11:27 -0300

    start heroku

---
+++ README.md
@@ -0,0 +1,9 @@
+# APY
+API para obtener un listado completo de los departamentos y ciudades del Paraguay
+
+Créditos a [@millan2993](https://github.com/millan2993) por los [datos](https://github.com/millan2993/countries)
+
+
+# Ciudades
+
+# Departamentos
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const states = require("./data/states.json");
+const cities = require("./data/cities.json");
+const app = express();
+const port = 3000;
+
+const departments = states.states.filter((s) => s.id_country === 110);
+
+app.get("/", (req, res) => {
+  res.send({
+    departments: departments.map((s) => {
+      return { id: s.id, name: s.name };
+    }),
+  });
+});
+
+app.get("/ciudades", (req, res) => {
+  res.send({
+    cities: cities.cities
+      .filter((c) => !!departments.some((d) => d.id === c.id_state))
+      .map((c) => {
+        return { id: c.id, department_id: c.id_state, name: c.name };
+      }),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
