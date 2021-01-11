08d2440f19612bda1238833d08055c949be88146
nandyme <fernandomellone2@gmail.com>
2021-01-11 01:17:30 -0300

    changing ports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const states = require("./data/states.json");
 const cities = require("./data/cities.json");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const departments = states.states.filter((s) => s.id_country === 110);
 
